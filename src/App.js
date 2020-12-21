2befc7c170f3615fc5fbda1c2daccaf6c287caa9
Aztec <salgorog@hotmail.com>
2020-12-20 21:48:58 -0700

    created the gallery component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
+import React from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
 
 function App() {
 
   return (
     <div>
+      <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
