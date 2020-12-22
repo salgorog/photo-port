ea0da137a733f09dea2140a3c745dab1f7b1c048
Aztec <salgorog@hotmail.com>
2020-12-21 21:13:42 -0700

    fixed error rendering

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
@@ -7,6 +7,9 @@ function Nav(props) {
     setCurrentCategory,
     currentCategory,
   } = props;
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
