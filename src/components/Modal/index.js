8eca88554f8975ab1ac6fb777b97783f74e06367
Aztec <salgorog@hotmail.com>
2021-01-16 01:03:24 -0700

    closing the modal working

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-function Modal() {
-
+function Modal({ onClose, currentPhoto }) {
     const { name, category, description, index } = currentPhoto;
 
     return (
         <div className="modalBackdrop">
             <div className="modalContainer">
                 <h3 className="modalTitle">{name}</h3>
-                <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
-                <p>{description}</p>
-                <button type="button">
+                <img src={require(`../../assets/large/${category}/${index}.jpg`).default} alt="current category" />
+                <p>
+                    {description}
+                </p>
+                <button onClick={onClose} type="button">
                     Close this modal
-          </button>
+                </button>
             </div>
         </div>
     );
